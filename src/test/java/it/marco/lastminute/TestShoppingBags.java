12f65cad63b023cf260f27b684109ed299a652d5
Marco <marco.berti@it.pwc.com>
2019-04-01 12:21:32 +0200

    New Test for Factory

---
+++ src/test/java/it/marco/lastminute/TestShoppingBags.java
@@ -0,0 +1,53 @@
+package it.marco.lastminute;
+
+import it.marco.lastminute.dto.Item;
+import it.marco.lastminute.dto.Receipt;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestShoppingBags {
+
+	/*
+	 * TODO
+	 * - classes
+	 * - constructors
+	 * - methods
+	 */
+
+	@Test
+	@DisplayName("Test Shopping Bag 1")
+	public void testShoppingBag1() {
+
+		ShoppingBag shoppingBag = new ShoppingBag();
+		FactoryInterface factory = new FirstShoppingBagFactory();
+
+		shoppingBag.doShopping(factory);
+
+		Receipt receipt = new Receipt(shoppingBag);
+
+		List<Item> itemList = shoppingBag.getItems();
+
+		assertEquals(BigDecimal.valueOf(12.49), itemList.get(0).getFinalPrice());
+		assertEquals(BigDecimal.valueOf(16.49), itemList.get(1).getFinalPrice());
+		assertEquals(BigDecimal.valueOf(0.85), itemList.get(2).getFinalPrice());
+
+		assertEquals(BigDecimal.valueOf(1.50).setScale(2), receipt.getTotalTaxesAmount());
+		assertEquals(BigDecimal.valueOf(29.83), receipt.getTotalAmount());
+
+		String printedReceipt = receipt.print();
+
+		assertEquals(Boolean.TRUE, printedReceipt.contains("Book"));
+		assertEquals(Boolean.TRUE, printedReceipt.contains("12.49"));
+		assertEquals(Boolean.TRUE, printedReceipt.contains("MusicCD"));
+		assertEquals(Boolean.TRUE, printedReceipt.contains("16.49"));
+		assertEquals(Boolean.TRUE, printedReceipt.contains("Chocolate"));
+		assertEquals(Boolean.TRUE, printedReceipt.contains("0.85"));
+		assertEquals(Boolean.TRUE, printedReceipt.contains("29.83"));
+		assertEquals(Boolean.TRUE, printedReceipt.contains("1.50"));
+	}
+}
