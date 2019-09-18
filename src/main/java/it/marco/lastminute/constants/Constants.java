1abba8ffdaf5264940e2115962785bd653a8760e
mberti <elChu/0000>
2019-09-18 16:07:17 +0200

    Delete all for REDO

--- src/main/java/it/marco/lastminute/constants/Constants.java
+++
@@ -1,7 +0,0 @@
-package it.marco.lastminute.constants;
-
-public class Constants {
-
-	public static final Integer TAX_FOR_TAXABLE_ITEM = 10;
-	public static final Integer TAX_FOR_IMPORTED_ITEM = 5;
-}
